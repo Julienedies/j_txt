fe2e1fd89ce5c9dde18031fd74a68a22af05e018
j <julienedies@gmail.com>
2017-04-16 13:32:27 +0800

    init

---
+++ index.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Julien on 2017/4/15.
+ */
+
+var program = require('commander');
+
+program
+    .version('1.0')
+    .usage('jtxt [options]')
+    .option('-r, --release <filename>', 'custom release.json filename')
+    .option('-m, --manifest <filename>', 'custom manifest.json filename')
+    .option('-c, --config <path>', 'json file path, custom fields, version etc.')
+    .parse(process.argv);
+
+
+
+var chcpContext = require('../lib/context.js');
+var command = require('../lib/build.js');
+
+command.execute(chcpContext.context({
+    r: program.release,
+    m: program.manifest,
+    c: program.config
+}));
+
+
+//对外接口
+module.exports = function(){
+
+};

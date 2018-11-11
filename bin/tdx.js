92e3d2e26bc5feaac6eb59a6db74fd11b1c0920b
julienedies <julienedies@gmail.com>
2018-11-11 16:42:52 +0800

    子命令 tdx 细节优化

--- bin/tdx.js
+++ bin/tdx.js
@@ -7,10 +7,8 @@ module.exports = function(program){
 
     program
         .command('tdx')
-        .description('通达信自定义数据输出')
-        .option('-d, --dist [file_name]', "通达信自定义数据输出文件,默认当前目录tdx.txt")
-        .option('-s, --stocks [股票数组]', "股票数组:[['000001','平安银行'],['002564','天沃科技']]")
-        .option('-c, --col [数据项]', "'概念', '概念y', '产品', '业务'")
+        .description('通达信自定义数据输出, example: jhandy tdx;  jhandy tdx -c 业务;')
+        .option('-c, --col [数据项]', "'概念', '概念y', '产品', '业务', 默认['概念', '概念y', '产品', '业务', '全名', '备注']")
         .action(function (cmd) {
             let c = cmd.col;
             let _tdx = require('./_tdx.js');

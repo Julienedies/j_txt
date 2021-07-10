035f645ad66fc0c0665de089fe63bfae47721e17
julienedies <julienedies@gmail.com>
2021-07-10 09:59:32 +0800

    optimize

--- src/libs/stock/fetch/ths_a.js
+++ src/libs/stock/fetch/ths_a.js
@@ -9,26 +9,32 @@ export default function (code) {
 
     return new Promise((resolve, reject) => {
 
-        let url =  `http://basic.10jqka.com.cn/mapp/${ code }/a_companies_list.json`;
+        let url = `http://basic.10jqka.com.cn/mapp/${ code }/a_companies_list.json`;
 
-        superagent.get(url).accept('json').end( (err, res) => {
+        superagent.get(url).accept('json').end((err, res) => {
             if (err) return reject(err);
-            let data = res.body.data;
-            let arr = data.domestic.company_data[0].list;
-            arr = arr.map((item, index) => {
-                return item.name;
-            });
-            let field = data.field;
 
-            /*let obj = {};
-            obj[field] = arr;
-            let result = {'同业': obj};*/
+            try {
+                let data = res.body.data;
+                let arr = data.domestic.company_data[0].list;
+                arr = arr.map((item, index) => {
+                    return item.name;
+                });
+                let field = data.field;
 
-            arr = arr.join('  ');
-            let result = {'同业': `${ field }: ${ arr }`};
+                /*let obj = {};
+                obj[field] = arr;
+                let result = {'同业': obj};*/
 
-            console.log(JSON.stringify(result, 'null', '\t'));
-            resolve({result, source_id: 'ths_a', code});
+                arr = arr.join('  ');
+                let result = {'同业': `${ field }:  ${ arr }`};
+
+                console.log(JSON.stringify(result, 'null', '\t'));
+                resolve({result, source_id: 'ths_a', code});
+            } catch (err) {
+                reject(err);
+            }
+
         });
 
     });
